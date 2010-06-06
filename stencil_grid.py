680f77dc3f0d420f8696826a393bb2922d744a47
Shoaib Kamil <skamil@cs.berkeley.edu>
2010-06-05 21:16:26 -0700

    Added grid_variables definition.  Added 3d case for interior iterator

--- stencil_grid.py
+++ stencil_grid.py
@@ -8,10 +8,39 @@ class StencilGrid(object):
         self.shape = size
         self.ghost_depth = 1
         self.interior = [x-2*self.ghost_depth for x in self.shape]
+        
+        self.grid_variables = ["DIM"+str(x) for x in range(0,self.dim)]
+
 
     def interior_points(self):
+        """
+        Iterator over the interior points of the grid.  Only executed
+        in pure Python mode; in SEJITS mode, it should be executed only
+        in the translated language/library.
+        """
         # just do 2 and 3 dimension cases for now
         if self.dim == 2:
             for x in range(self.ghost_depth,self.shape[0]-self.ghost_depth):
                 for y in range(self.ghost_depth,self.shape[1]-self.ghost_depth):
                     yield [x,y]
+        elif self.dim == 3:
+            for x in range(self.ghost_depth,self.shape[0]-self.ghost_depth):
+                for y in range(self.ghost_depth,self.shape[1]-self.ghost_depth):
+                    for z in range(self.ghost_depth,self.shape[2]-self.ghost_depth):
+                        yield [x,y,z]
+
+    def border_points(self):
+        """
+        Iterator over the border points of a grid.  Only executed in pure Python
+        mode; in SEJITS mode, it should be executed only in the translated
+        language/library.
+        """
+        pass
+
+
+    def neighbors(center, dist):
+        """
+        Returns a list of neighbors that are at distance dist from the center
+        point.  Uses neighbor_definition to determine what the neighbors are.
+        """
+        pass
--- stencil_grid_test.py
+++ stencil_grid_test.py
@@ -7,12 +7,21 @@ class BasicTests(unittest.TestCase):
         self.failIf(grid.dim != 2)
         self.failIf(grid.data.shape != (10,10))
         self.failIf(grid.interior != [8,8])
+        self.failIf(grid.grid_variables[0] != "DIM0")
 
-    def test_iterator(self):
+    def test_interior_iterator(self):
+        # 2D
         grid = StencilGrid([5,5])
         pts = [x for x in grid.interior_points()]
         self.failIf(pts[0] != [1,1])
         self.failIf(len(pts) != 9)
+        # 3D
+        grid = StencilGrid([5,5,5])
+        pts = [x for x in grid.interior_points()]
+        self.failIf(pts[0] != [1,1,1])
+        self.failIf(len(pts) != 27)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
