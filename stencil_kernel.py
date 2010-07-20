4bcae21b1bd63a58057a2b1c50c00b88f9818e5b
Shoaib Kamil <skamil@cs.berkeley.edu>
2010-07-20 16:09:11 -0700

    classes to process AST into stencil-specific AST

--- stencil_kernel.py
+++ stencil_kernel.py
@@ -2,6 +2,10 @@ import numpy
 import inspect
 from stencil_grid import *
 import simple_ast
+import ast
+
+# Overall flow: StencilProcessAST (now has stencil node) ---> StencilToCAST (now is a C++ AST) ---> codegen
+
 
 # may want to make this inherit from something else...
 class StencilKernel(object):
@@ -23,6 +27,7 @@ class StencilKernel(object):
 
         # replace kernel with shadow version
         self.kernel = self.shadow_kernel
+        
 
     def remove_indentation(self, src):
         return src.lstrip()
@@ -31,55 +36,112 @@ class StencilKernel(object):
         #FIXME: need to somehow match arg names to args
         argnames = map(lambda x: str(x.id), self.kernel_ast.body[0].args.args)
         argdict = dict(zip(argnames[1:], args))
-        cg = self.StencilCodegen(argdict)
-        print cg.visit(self.kernel_ast)
+        print argdict
+        #cg = self.StencilCodegen(argdict)
+        print ast.dump(StencilKernel.StencilProcessAST(argdict).visit(self.kernel_ast))
 
 
+    # the actual Stencil AST Node
+    class StencilInteriorIter(ast.AST):
+        def __init__(self, grid, body, target):
+          self.grid = grid
+          self.body = body
+          self.target = target
+          self._fields = ('grid', 'body', 'target')
 
-    # class to codegen stencils
-    import codegen
-    class StencilCodegen(codegen.CodeGenerator):
-        
-        def __init__(self, argdict):
-            #FIXME: should support multiple input arrays
-            #FIXME: should reverse order?
-            self.argdict = argdict
-            super(StencilKernel.StencilCodegen, self).__init__()
-
-        
-        def gensym(self):
-            """ Generates random strings for unique identifiers. """
-            import random, string
-            return "_i" + "".join(random.sample(string.letters+string.digits, 8))
-
-        def visit_For(self, node):
+          super(StencilKernel.StencilInteriorIter, self).__init__()
             
+    class StencilNeighborIter(ast.AST):
+        def __init__(self, grid, body, target, dist):
+            self.grid = grid
+            self.body = body
+            self.target = target
+            self.dist = dist
+            self._fields = ('grid', 'body', 'target', 'dist')
+            super (StencilKernel.StencilNeighborIter, self).__init__()
+
+
+    # class to convert from Python AST to an AST with special Stencil node
+    class StencilProcessAST(ast.NodeTransformer):
+        def __init__(self, argdict):
+            self.argdict = argdict
+            super(StencilKernel.StencilProcessAST, self).__init__()
+
+        
+        def visit_For(self, node):
+            print "visiting a For...\n"
+            # check if this is the right kind of For loop
             if (node.iter.__class__.__name__ == "Call" and
                 node.iter.func.__class__.__name__ == "Attribute"):
+                
+                print "Found something to change...\n"
 
                 if (node.iter.func.attr == "interior_points"):
-                    grid_shape =  eval(self.visit(node.iter.func.value) + ".shape", self.argdict)
-                    grid_dim = len(grid_shape)
+                    grid = self.visit(node.iter.func.value)    # do we need the name of the grid, or the obj itself?
                     target = self.visit(node.target)
+                    body = map(self.visit, node.body)
+                    newnode = StencilKernel.StencilInteriorIter(grid, body, target)
+                    return newnode
+
+                elif (node.iter.func.attr == "neighbors"):
+                    print ast.dump(node) + "\n"
+                    target = self.visit(node.target)
+                    body = map(self.visit, node.body)
+                    grid = self.visit(node.iter.func.value)
+                    dist = self.visit(node.iter.args[0])
+                    newnode = StencilKernel.StencilNeighborIter(grid, body, target, dist)
+                    return newnode
+
+                else:
+                    return node
+            else:
+                return node
+
+
+#     # class to codegen stencils
+#     import codegen
+#     class StencilCodegen(codegen.CodeGenerator):
+        
+#         def __init__(self, argdict):
+#             #FIXME: should support multiple input arrays
+#             #FIXME: should reverse order?
+#             self.argdict = argdict
+#             super(StencilKernel.StencilCodegen, self).__init__()
+
+        
+#         def gensym(self):
+#             """ Generates random strings for unique identifiers. """
+#             import random, string
+#             return "_i" + "".join(random.sample(string.letters+string.digits, 8))
+
+#         def visit_For(self, node):
+            
+#             if (node.iter.__class__.__name__ == "Call" and
+#                 node.iter.func.__class__.__name__ == "Attribute"):
+
+#                 if (node.iter.func.attr == "interior_points"):
+#                     grid_shape =  eval(self.visit(node.iter.func.value) + ".shape", self.argdict)
+#                     grid_dim = len(grid_shape)
+#                     target = self.visit(node.target)
                     
-                    if grid_dim == 2:
+#                     if grid_dim == 2:
 
-                        i1 = self.gensym()
-                        i2 = self.gensym()
-                        self.grid_vars = [i1,i2]
-                        str = "\nfor (int %s=1; %s < %d; %s++){\n " % (i1,i1,grid_shape[0],i1)
-                        str += "for (int %s=1; %s < %d; %s++){\n " % (i2,i2,grid_shape[0],i2)
-                        # now let iterator var = proper index
-                        str += "int " + target + " = _INDEX(" + ','.join(self.grid_vars) + ");\n"
-                        str += ';'.join(map(self.visit, node.body))
-                        str += ";} }"
+#                         i1 = self.gensym()
+#                         i2 = self.gensym()
+#                         self.grid_vars = [i1,i2]
+#                         str = "\nfor (int %s=1; %s < %d; %s++){\n " % (i1,i1,grid_shape[0],i1)
+#                         str += "for (int %s=1; %s < %d; %s++){\n " % (i2,i2,grid_shape[0],i2)
+#                         # now let iterator var = proper index
+#                         str += "int " + target + " = _INDEX(" + ','.join(self.grid_vars) + ");\n"
+#                         str += ';'.join(map(self.visit, node.body))
+#                         str += ";} }"
                         
-                        return str
-                if (node.iter.func.attr == "neighbors"):
-                    # read the neighbors out
-                    return super(StencilKernel.StencilCodegen, self).vist_For(node)
+#                         return str
+#                 if (node.iter.func.attr == "neighbors"):
+#                     # read the neighbors out
+#                     return super(StencilKernel.StencilCodegen, self).vist_For(node)
 
-            return super(StencilKernel.StencilCodegen, self).visit_For(node)
+#             return super(StencilKernel.StencilCodegen, self).visit_For(node)
 
 
 
--- stencil_kernel_test.py
+++ stencil_kernel_test.py
@@ -24,7 +24,18 @@ class BasicTests(unittest.TestCase):
 class IntrospectionTests(unittest.TestCase):
     def test_get_kernel_body(self):
         pass
+    def test_simple_kernel(self):
+        class MyKernel(StencilKernel):
+            def kernel(self, in_grid, out_grid):
+                for x in out_grid.interior_points():
+                    for y in in_grid.neighbors(x, 1):
+                        out_grid[x] += in_grid[y]
 
+
+        kernel = MyKernel()
+        in_grid = StencilGrid([10,10])
+        out_grid = StencilGrid([10,10])
+        kernel.kernel(in_grid, out_grid)
 
 if __name__ == '__main__':
     unittest.main()
