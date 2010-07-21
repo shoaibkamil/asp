bae1ee99f93db84162f518971ad62027d69260df
Shoaib Kamil <skamil@cs.berkeley.edu>
2010-07-21 15:53:48 -0700

    start of new codegen machinery

--- codegen.py
+++ codegen.py
@@ -1,70 +1,80 @@
-import ast
+
+from codepy.cgen import *
+
+# class to convert from python AST to C++ AST
 
 
-### Code generation
-class CodeGenerator(ast.NodeVisitor):
-    def generic_visit(self, node):
-        str = ""
-
-        import sys
-        sys.stderr.write("Unsupported node: " + node.__class__.__name__ + "\n")
-        
-        for child in ast.iter_child_nodes(node):
-            str += self.visit(child)
-
-        return str
-
-    # numbers/names/etc
-    def visit_Num(self, node):
-        return str(node.n)
-    def visit_Name(self, node):
-        return str(node.id)
-    def visit_Pass(self, node):
-        return ";"
-
-    # function call
-    def visit_Call(self, node):
-        func = self.visit(node.func)
-        # map visit() onto the parameters and join them with commas
-        args = ','.join(map(self.visit, node.args))        
-        return func + "(" + args + ")"
-
-    # attribute
-    # does foo.bar
-    def visit_Attribute(self, node):
-        return self.visit(node.value) + "." + node.attr
-
-    # subscript
-    def visit_Subscript(self, node):
-        return self.visit(node.value) + self.visit(node.slice)
-    # kinds of subscripts
-    def visit_Index(self, node):
-        return "[%s]" % self.visit(node.value)
 
 
-    # Binary operations
-    def visit_BinOp(self, node):
-        return self.visit(node.left) + self.visit(node.op) + self.visit(node.right)
+# classes to express everything in C++ AST
 
-    def visit_Add(self, node):
-        return "+"
-    def visit_Mult(self, node):
-        return "*"
-    def visit_Sub(self, node):
-        return "-"
-    def visit_Div(self, node):
-        return "/"
+class Expression(Generable):
+	def __str__(self):
+		raise NotImplementedError
 
-    # assignement
-    # only support a single assignment by default
-    def visit_Assign(self, node):
-        return self.visit(node.targets[0]) + " = " + self.visit(node.value)
+	def generate(self):
+		yield str(self) + ';'
 
+class BinOp(Expression):
+	def __init__(self, left, op, right):
+		self.left = left
+		self.op = op
+		self.right = right
 
-    # for iterator
-    def visit_For(self, node):
-        str =  "for " + self.visit(node.target)
-        str += " in " +  self.visit(node.iter) + " {\n"
-        str +=  ";".join(map(self.visit, node.body)) + "\n}"
-        return str
-                                   
+	def __str__(self):
+		return "(%s %s %s)" % (self.left, self.op, self.right)
+
+class UnaryOp(Expression):
+	def __init__(self, op, operand):
+		self.op = op
+		self.operand = operand
+
+	def __str__(self):
+		return "(%s(%s))" % (self.op, self.operand)
+
+class Subscript(Expression):
+	def __init__(self, value, index):
+		self.value = value
+		self.index = index
+
+	def __str__(self):
+		return "%s[%s]" % (self.value, self.index)
+
+class Call(Expression):
+	def __init__(self, func, args):
+		self.func = func
+		self.args = args
+
+	def __str__(self):
+		return "%s(%s)" % (self.func, ", ".join(map(str, self.args)))
+
+# expression types not used in this example:
+
+class PostfixUnaryOp(Expression):
+	def __init__(self, operand, op):
+		self.operand = operand
+		self.op = op
+
+	def __str__(self):
+		return "((%s)%s)" % (self.operand, self.op)
+
+class ConditionalExpr(Expression):
+	def __init__(self, test, body, orelse):
+		self.test = test
+		self.body = body
+		self.orelse = orelse
+
+	def __str__(self):
+		return "(%s ? %s : %s)" % (self.test, self.body, self.orelse)
+
+class TypeCast(Expression):
+	# "type" should be a declaration with an empty variable name
+	# e.g. TypeCast(Pointer(Value('int', '')), ...)
+
+	def __init__(self, type, value):
+		self.type = type
+		self.value = value
+
+	def __str__(self):
+		return "((%s)%s)" % (self.type.inline(), self.value)
+
