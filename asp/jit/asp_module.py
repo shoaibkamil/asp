992fde6cb03461e7869e893291696c7c5c730905
Shoaib Kamil <skamil@cs.berkeley.edu>
2010-11-01 16:26:34 -0700

    Added capability to use strings instead of AST nodes for ASPModule functions.

--- asp/jit/asp_module.py
+++ asp/jit/asp_module.py
@@ -1,12 +1,14 @@
 import codepy, codepy.jit, codepy.toolchain, codepy.bpl
 from asp.util import *
+import asp.codegen.cpp_ast as cpp_ast
+
 
 class ASPModule(object):
+
     
     def __init__(self):
         self.toolchain = codepy.toolchain.guess_toolchain()
         self.module = codepy.bpl.BoostPythonModule()
-        self.program_text = None
 
     def add_library(self, feature, include_dirs, library_dirs=[], libraries=[]):
         self.toolchain.add_library(feature, include_dirs, library_dirs, libraries)
@@ -15,24 +17,32 @@ class ASPModule(object):
         import asp.codegen.cpp_ast as cpp_ast
         self.module.add_to_preamble([cpp_ast.Include(include_file, False)])
 
+    def add_to_preamble(self, pa):
+        if isinstance(pa, str):
+            pa = cpp_ast.Line(pa)
+        self.module.add_to_preamble(pa)
+
     def add_to_init(self, stmt):
+        if isinstance(stmt, str):
+            stmt = cpp_ast.Line(stmt)
         self.module.add_to_init(stmt)
 
-    def add_function(self, func):
+    def add_function(self, func, fname=None):
         """
         self.add_function(func) takes func as either a generable AST or a string.
         """
-        if func.__class__ == str:
-            self.program_text = func
+        if isinstance(func, str):
+            if fname == None:
+                raise Exception("Cannot add a function as a string without specifying the function's name")
+            self.module.add_to_module([cpp_ast.Line(func)])
+            self.module.add_to_init([cpp_ast.Statement(
+                        "boost::python::def(\"%s\", &%s)" % (fname, fname))])
         else:
             self.module.add_function(func)
-            self.program_text = self.module.generate()
-
-        debug_print(self.program_text)
+            
+            
 
     def compile(self):
-        if self.program_text == None:
-            raise Exception("No code to generate.")
         
         self.compiled_module = self.module.compile(self.toolchain, debug=True, cache_dir=".")
         
