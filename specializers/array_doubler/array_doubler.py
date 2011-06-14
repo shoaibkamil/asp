f01a231b71e7e81b06481ab22d774f51f4d533ff
Scott Beamer <sbeamer@eecs.berkeley.edu>
2011-06-14 14:31:54 -0700

    tweak to ignore unicode for mako

--- specializers/array_doubler/array_doubler.py
+++ specializers/array_doubler/array_doubler.py
@@ -7,7 +7,7 @@ class ArrayDoubler(object):
 
     def double_using_template(self, arr):
         import asp.codegen.templating.template as template
-        mytemplate = template.Template(filename="templates/double_template.mako")
+        mytemplate = template.Template(filename="templates/double_template.mako", disable_unicode=True)
         rendered = mytemplate.render(num_items=len(arr))
 
         import asp.jit.asp_module as asp_module
