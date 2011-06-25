b579b933c3c23ec2b13822910d42d19fd83fe62c
Shoaib Kamil <skamil@cs.berkeley.edu>
2011-06-25 15:50:55 -0700

    Cleanup no-longer needed code.

--- asp/jit/asp_module.py
+++ asp/jit/asp_module.py
@@ -303,60 +303,6 @@ class ASPModule(object):
         return func.fdecl.subdecl.name
 
 
-#    def add_function_helper(self, func, fname=None, cuda_func=False, backend="c++"):
-        #FIXME: want to deprecate cuda_func parameter.  this should just pickup the module
-        #from the backend parameter.
-
-        # OLD code:
-        # if cuda_func:
-        #     module = self.backends["cuda"].module
-        # else:
-        #     module = self.backends["c++"].module
-        
-        # if isinstance(func, str):
-        #     if fname == None:
-        #         raise Exception("Cannot add a function as a string without specifying the function's name")
-        #     module.add_to_module([cpp_ast.Line(func)])
-        #     module.add_to_init([cpp_ast.Statement(
-        #                 "boost::python::def(\"%s\", &%s)" % (fname, fname))])
-        # else:
-        #     module.add_function(func)
-        # self.dirty = True
-
-
-        
-
-    # def add_function_with_variants(self, variant_funcs, func_name, variant_names, key_maker=lambda name, *args, **kwargs: (name), limit_funcs=None, compilable=None, param_names=None, cuda_func=False):
-        
-    #     limit_funcs = limit_funcs or [lambda name, *args, **kwargs: True]*len(variant_names) 
-    #     compilable = compilable or [True]*len(variant_names)
-    #     param_names = param_names or ['Unknown']*len(variant_names)
-    #     method_info = self.compiled_methods.get(func_name, None)
-    #     if not method_info:
-    #         method_info = CodeVariants(variant_names, key_maker, param_names)
-    #         method_info.limiter.append(variant_names, limit_funcs, compilable)
-    #     else:
-    #         method_info.append(variant_names)
-    #         method_info.database.clear_oracle()
-    #         method_info.limiter.append(variant_names, limit_funcs, compilable)
-    #     for x in range(0,len(variant_funcs)):
-    #         self.add_function_helper(variant_funcs[x], fname=variant_names[x], cuda_func=cuda_func)
-    #     self.compiled_methods[func_name] = method_info
-
-    # def add_function(self, funcs, fname=None, variant_names=None, cuda_func=False):
-    #     """
-    #     self.add_function(func) takes func as either a generable AST or a string, or
-    #     list of variants in either format.
-    #     """
-    #     if variant_names:
-    #         self.add_function_with_variants(funcs, fname, variant_names, cuda_func=cuda_func)
-    #     else:
-    #         variant_funcs = [funcs]
-    #         if not fname:
-    #             fname = self.get_name_from_func(funcs)
-    #         variant_names = [fname]
-    #         self.add_function_with_variants(variant_funcs, fname, variant_names, cuda_func=cuda_func)
-
     def add_function(self, fname, funcs, variant_names=None, run_check_function=None, key_function=None, 
                      backend="c++"):
         """
@@ -378,64 +324,8 @@ class ASPModule(object):
         Add a helper function, which is a specialized function that it not timed and has a single variant.
         """
         self.specialized_functions[fname] = HelperFunction(fname, func, self.backends[backend])
-        
 
 
-                
-#    def compile(self):
-#        if self.use_cuda:
-#            self.compiled_module = self.backends["cuda"].module.compile(self.backends["c++"].module,
-#                                                                        self.backends["cuda"].toolchain,
-#                                                                        debug=True, cache_dir=self.cache_dir)
-#        else:
-#            self.compiled_module = self.backends["c++"].module.compile(self.backends["c++"].toolchain,
-#                                                                       debug=True, cache_dir=self.cache_dir)
-#        self.dirty = False
-#        
-#    def specialized_func(self, name):
-#        import time
-#        def error_func(*args, **kwargs):
-#            raise Exception("No variant of method found to run on input size %s on the specified device" % str(args))
-#        def special(*args, **kwargs):
-#            method_info = self.compiled_methods[name]
-#            key = method_info.make_key(name,*args,**kwargs)
-#            v_id = method_info.selector.get_v_id_to_run(method_info.v_id_set, key,*args,**kwargs)
-#            real_func = self.compiled_module.__getattribute__(v_id) if v_id else error_func
-#            start_time = time.time() 
-#            result = real_func(*args, **kwargs)
-#            elapsed = time.time() - start_time
-#            method_info.database.add_time( key, elapsed, v_id, method_info.v_id_set)
-#            return result
-#        return special
-#
-#    def helper_func(self, name):
-#        def helper(*args, **kwargs):
-#            real_func = self.compiled_module.__getattribute__(name)
-#            return real_func(*args, **kwargs)
-#        return helper
-#
-#    def save_method_timings(self, name, file_name=None):
-#        method_info = self.compiled_methods[name]
-#        f = open(file_name or self.cache_dir+'/'+name+'.vardump', 'w')
-#        d = method_info.get_picklable_obj()
-#        d.update(method_info.database.get_picklable_obj())
-#        pickle.dump( d, f)
-#        f.close()
-#
-#    def restore_method_timings(self, name, file_name=None):
-#        method_info = self.compiled_methods[name]
-#        try: 
-#	    f = open(file_name or self.cache_dir+'/'+name+'.vardump', 'r')
-#            obj = pickle.load(f)
-#            if obj: method_info.set_from_pickled_obj(obj)
-#            if obj: method_info.database.set_from_pickled_obj(obj, method_info.v_id_set)
-#            f.close()
-#        except IOError: pass
-#
-#    def clear_method_timings(self, name):
-#        method_info = self.compiled_methods[name]
-#        method_info.database.clear()
-#
     def __getattr__(self, name):
         if name in self.specialized_functions:
             if self.dirty:
