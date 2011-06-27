35e0c97b648dd268105423670b483ee85989c4c0
Shoaib Kamil <skamil@cs.berkeley.edu>
2011-06-27 14:50:58 -0700

    added detection for presence of compilers.

--- asp/config.py
+++ asp/config.py
@@ -1,31 +1,48 @@
 import re
 
+class CompilerDetector(object):
+    """
+    Detect if a particular compiler is available by trying to run it.
+    """
+    def detect(self, compiler):
+        import subprocess
+        try:
+            retcode = subprocess.call([compiler, "--version"])
+        except:
+            return False
+
+        return (retcode == 0)
+        
+
 class PlatformDetector(object):
     def __init__(self):
         self.rawinfo = []
 
-    def getGPUInfo(self):
+    def get_gpu_nfo(self):
         raise NotImplementedError
 
-    def getCPUInfo(self):
-        self.rawinfo = self.readCPUInfo()
+    def get_cpu_info(self):
+        self.rawinfo = self.read_cpu_info()
         info = {}
-        info['numCores'] = self.parseNumCores()
-        info['vendorID'] = self.parseCPUInfo('vendor_id')
-        info['model'] = int(self.parseCPUInfo('model'))
-        info['cpuFamily'] = int(self.parseCPUInfo('cpu family'))
-        info['cacheSize'] = int(self.parseCPUInfo('cache size'))
-        info['capabilities'] = self.parseCapabilities()
+        info['numCores'] = self.parse_num_cores()
+        info['vendorID'] = self.parse_cpu_info('vendor_id')
+        info['model'] = int(self.parse_cpu_info('model'))
+        info['cpuFamily'] = int(self.parse_cpu_info('cpu family'))
+        info['cacheSize'] = int(self.parse_cpu_info('cache size'))
+        info['capabilities'] = self.parse_capabilities()
         return info
 
-    def parseCapabilities(self):
+    def get_compilers(self):
+        return filter(CompilerDetector().detect, ["gcc", "icc", "nvcc"])
+
+    def parse_capabilities(self):
         matcher = re.compile("flags\s+:")
         for line in self.rawinfo:
             if re.match(matcher, line):
                 return line.split(":")[1].split(" ")
     
         
-    def parseNumCores(self):
+    def parse_num_cores(self):
         matcher = re.compile("processor\s+:")
         count = 0
         for line in self.rawinfo:
@@ -33,11 +50,11 @@ class PlatformDetector(object):
                 count +=1
         return count
         
-    def parseCPUInfo(self, item):
+    def parse_cpu_info(self, item):
         matcher = re.compile(item +"\s+:\s*(\w+)")
         for line in self.rawinfo:
             if re.match(matcher, line):
                 return re.match(matcher, line).group(1)
         
-    def readCPUInfo(self):
+    def read_cpu_info(self):
         return open("/proc/cpuinfo", "r").readlines()
