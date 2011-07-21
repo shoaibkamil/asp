96aa63a543d4186eb3d0da535823e60efbabe038
Shoaib Kamil <skamil@cs.berkeley.edu>
2011-07-21 16:43:52 -0700

    Added actualy test assertion to 3d testcase.

--- specializers/stencil/tests/stencil_cache_block_test.py
+++ specializers/stencil/tests/stencil_cache_block_test.py
@@ -47,8 +47,24 @@ class CacheBlockerTests(unittest.TestCase):
                                                            CNumber(1),
                                                            Block(contents=[Assign(CName("v"), CName("i"))]))]))]))
 
-        print StencilCacheBlocker().block(loop, (2,2,3))
-        assert(False)
+        #print StencilCacheBlocker().block(loop, (2,2,3))
+        wanted = """for (int ii = 0; (ii <= 7); ii = (ii + (1 * 2)))
+        {
+        for (int jj = 0; (jj <= 3); jj = (jj + (1 * 2)))
+        {
+        for (int kk = 0; (kk <= 4); kk = (kk + (1 * 3)))
+        {
+        for (int i = ii; (i <= min((ii + 2),7)); i = (i + 1))
+        {
+        for (int j = jj; (j <= min((jj + 2),3)); j = (j + 1))
+        {
+        for (int k = kk; (k <= min((kk + 2),4)); k = (k + 1))
+        {
+        v = i;
+        }\n}\n}\n}\n}\n}"""
+        self.assertEqual(str(StencilCacheBlocker().block(loop, (2,2,3))).replace(' ',''),
+                         wanted.replace(' ', ''))
+
         
 if __name__ == '__main__':
     unittest.main()
