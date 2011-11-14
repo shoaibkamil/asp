aa3cb04a3f6e14972adedb3319a57be39ea72d64
Derrick Coetzee <dc@moonflare.com>
2011-11-14 03:53:53 -0800

    Go back to using discover - does work in Python 2.6, but need to use unit2 script

--- specializers/stencil/run_tests.sh
+++ specializers/stencil/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PYTHONPATH=../../:tests:$PYTHONPATH
+PYTHONPATH=../../:$PYTHONPATH
 
 if [ -z "${PYTHON}" ]
 then
@@ -11,13 +11,4 @@ then
 fi
 
 rm -fr /tmp/asp_cache
-PYTHONPATH=`pwd`:${PYTHONPATH} ${PYTHON} ${PYTHONARGS} -m unittest \
-assert_utils_test \
-stencil_cache_block_test \
-stencil_convert_test \
-stencil_grid_test \
-stencil_kernel_test \
-stencil_model_interpreter_test \
-stencil_model_test \
-stencil_python_front_end_test \
-stencil_unroll_neighbor_iter_test
+PYTHONPATH=`pwd`:${PYTHONPATH} unit2 discover tests '*.py'
