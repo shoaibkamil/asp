82fc92ed6b2b4056615c9e2fe077ff0893bcab4e
Jessica <jvmiller224@gmail.com>
2011-12-10 00:11:13 -0800

    config file test case

---
+++ tests/config_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from asp.config import *
+
+class ConfigReaderTest(unittest.TestCase):
+    def test_getOption(self):
+        self.assertNotEqual(ConfigReader().get_option('gmm'), None)
+        self.assertEqual(CompilerDetector().get_option('qwerty'), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
