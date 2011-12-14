ad974e0b181e8c6696fec72cb369ffb2b0294da4
Shoaib Kamil <skamil@cs.berkeley.edu>
2011-12-14 14:14:44 -0800

    Installation changes for 0.1.2.1, which will become 0.1.3 if no problems are found.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name="asp",
       author_email="skamil@cs.berkeley.edu",
       license = "BSD",
 
-      packages=["asp", "asp.codegen", "asp.codegen.templating", "asp.jit"],
+      packages=["asp", "asp.codegen", "asp.codegen.templating", "asp.jit", "asp.ply"],
       install_requires=[
         "numpy",
         "codepy",
