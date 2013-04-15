8e968dd71ea0a340fece5deac7d7bc00f1e99825
Derrick Coetzee <dcoetzee@eecs.berkeley.edu>
2013-04-15 07:30:52 -0700

    Update to deal with timeout and new distribute prerequisite of numpy

--- install/ubuntu_12.04.sh
+++ install/ubuntu_12.04.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get install python-pip python-dev libboost-python-dev g++
-sudo pip install asp
+sudo easy_install -U distribute
+sudo pip --default-timeout=1000 install --use-mirrors asp
 echo "BOOST_PYTHON_LIBNAME=['boost_python-mt-py27']" >> ~/.aksetup-defaults.py
